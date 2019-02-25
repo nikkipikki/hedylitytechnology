a4ae652e6a79601f695157d665433e7e9821c105
Nicole Pehar <nicolepehar@nicoles-air.lan>
2019-02-25 09:45:59 +0100

    japp

--- src/components/hedypage/index.js
+++ src/components/hedypage/index.js
@@ -11,8 +11,7 @@ class Academypage extends React.Component {
     return (
       <div className="fadeinclass">
         <ScrollToTopOnMount/>
-        <div>
-        </div>
+      <div className="mobile">
         <div className="pagemargin">
 
         <div className="whatwedoheadertext">
@@ -45,8 +44,42 @@ class Academypage extends React.Component {
           <p>Just as Hedy, Hedylity Technology wants to
           make a tech imprint for a better world</p>
         </div>
-
         </div>
+      </div>
+
+
+        <div className="hedypagewebbox">
+        <div className="whatwedoheadertext">
+          <h1>HEDY LAMARR</h1>
+        </div>
+        <div className="hedylamarrtext">
+          <p>Hedylity Technology is named after Hedy Lamarr,
+          the brain behind frequency hopping.
+          Her invention came from wanting to think of something to stop Hitler
+          from taking over Europe during World War II.</p>
+          <p>Frequency hopping is the basis for secure Wi-Fi, Bluetooth,
+          cell phone, GPS and military technology –
+          an invention that forms our whole society today!</p>
+          <p>Hedy was a famous Hollywood actor and the inspiration behind the
+          look of Snow-white and Catwoman, she never got any share of
+          her 30 billion dollar invention.</p>
+        </div>
+        <div className="hedypicturebox" id="hedylamarrpicture">
+        <h1 id="hedylamarrquote">“The brains of people are more
+        interesting than the looks”</h1>
+        <h1 className="hedyquotesmalltext">Hedy Lamarr (1914 – 2000)</h1>
+        </div>
+
+
+
+        <div className="hedypicturebox" id="hedyhandsvartbeige"/>
+        <div className="whatwedosmalltext">
+          <p>Just as Hedy, Hedylity Technology wants to
+          make a tech imprint for a better world</p>
+        </div>
+      </div>
+
+
 
       </div>
     )

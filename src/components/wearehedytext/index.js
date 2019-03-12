0ef10e31c431d9c4756ea053c5a814873de7e7e3
Nicole Pehar <nicolepehar@Nicoles-Air.lan>
2019-03-12 01:06:20 +0100

    stuff

--- src/components/wearehedytext/index.js
+++ src/components/wearehedytext/index.js
@@ -6,8 +6,10 @@ class Wearehedytext extends React.Component {
     return (
       <div>
           <div className="wearehedytextbox">
+          <div id="wearehedy">
             <p className="wearehedytext">We are Honest. We are Effectual.
             We are Devoted. We are Young & Witty. We are HEDY.</p>
+          </div>
           </div>
       </div>
     )
